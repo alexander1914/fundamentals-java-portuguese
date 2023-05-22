fbcc29dca4d55cbaad206b640a46f88d8a93ca5b
Alexander Oliveira <acc01719@sky.com.br>
2023-05-22 20:54:08 -0300

    Variaveis

---
+++ fundamentals-in-java/src/br/com/udemy/variaveis/Variaveis.java
@@ -0,0 +1,21 @@
+package br.com.udemy.variaveis;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        //TODO: Variáveis
+        // Declaração de variável
+
+        int idade = 32;
+        char caractere = 'A';
+        float preco = 199.99f;
+        double salario = 10.000;
+
+        System.out.println(idade);
+        System.out.println(caractere);
+        System.out.println(preco);
+        System.out.println(salario);
+
+        //TODO Exemplo: de uma comcatenização
+        System.out.println("Eu tenho " + idade + " anos");
+    }
+}
