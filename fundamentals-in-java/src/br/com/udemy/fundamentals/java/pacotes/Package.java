afda4c2961b30ee821bda438f65c17e65bf1f426
Alexander Oliveira <acc01719@sky.com.br>
2023-06-14 22:09:07 -0300

    Revisando os conceitos de package

---
+++ fundamentals-in-java/src/br/com/udemy/fundamentals/java/pacotes/Package.java
@@ -0,0 +1,13 @@
+//TODO: Pacotes: servem para organizar o código fonte do projeto,
+// e indicam onde determinado arquivo do projeto se encontra,
+// regras: os nomes dos pacotes devem ser em letras minúsculas, sem espaço,
+// sem caractere especial e sem acentuação.
+
+package br.com.udemy.fundamentals.java.pacotes;
+//TODO: Por convenção: é utilizado nos nomes de pacotes, o domínio da empresa criadora do projeto
+// ao contrário, sendo a última parte do nome o especificador do pacote.
+public class Package {
+    public static void main(String[] args) {
+
+    }
+}
